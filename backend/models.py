d613a55c9c5cb382c286736af9ee6c75be532831
Bren <bbarletta@fi.uba.ar>
2024-06-24 12:01:35 -0300

    Agregando modelos de tablas para la base de datos de la aplicación y modiicando la estructura del directorio para separar cada página que servirá el servidor frontend por carpetas.
    Archivos Agregados:
    - backend/models.py
    
    Archivos Modificados (cambios menores en el manejo de rutas, y cambios en su ubicación):
    - frontend/login-register/register.html
    - frontend/register/register.html

---
+++ backend/models.py
@@ -0,0 +1,26 @@
+import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(25), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+class Task(db.Model):
+    __tablename__ = "tasks"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    task_text = db.Column(db.Text, nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+class DailyGoal(db.Model):
+    __tablename__ = "daily_goals"
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    goal = db.Column(db.Text, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    date = db.Column(db.Date, nullable=False, default=datetime.date.today)
